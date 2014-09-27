f85b2ac986ce7b7ae68d7a357594f44c12dfe9e8
smehemmed <msxiyev@gmail.com>
2014-09-28 00:24:29 +0300

    Updated Sayi_Basamak.c example

--- my C examples/Sayi_Basamak.c
+++ my C examples/Sayi_Basamak.c
@@ -8,33 +8,34 @@ int birler(int);
 int main() {
 	int sayi;
 	printf("4 Basamakli Sayi Giriniz: "); scanf("%d", &sayi);
-	if (sayi < 10000 || sayi > 99999)
+	if (sayi < 1000 || sayi > 9999)
 	{
 		printf("Lutfen 4 basamakli sayi giriniz!!!\a");
 	} 
 	else
 	{
-		printf("Binler = %d" ,binler(sayi));
-		printf("Yuzler = %d" ,yuzler(sayi));
-		printf("Onlar  = %d" ,onlar(sayi));
-		printf("Birler = %d" ,birler(sayi));
+		printf("Binler = %d\n" ,binler(sayi));
+		printf("Yuzler = %d\n" ,yuzler(sayi));
+		printf("Onlar  = %d\n" ,onlar(sayi));
+		printf("Birler = %d\n" ,birler(sayi));
 	}
 	return 0;
 }
 int binler(int x) {
-	return(x\1000);
+	return x/1000;
 }
 int yuzler(int x) {
 	int a;
 	a = x%1000;
-	return a\100;
+	return a/100;
 }
 
 int onlar(int x) {
 	int a,b;
 	a = x % 1000;
 	b = a % 100;
-	return b\10;
+	return b/10;
 }
-int birler(int x) 
-	return x % 10 
+int birler(int x) {
+	return x % 10;
+}
