6b219b593717c77718b0e2e7b0411a799fb4568c
smehemmed <msxiyev@gmail.com>
2014-10-09 23:53:37 +0300

    added Bubble_sort.c

---
+++ my C examples/Bubble_sort.c
@@ -0,0 +1,14 @@
+/*Bubble sort kabarcık sıralama*/
+void Bubble_sort(int dizi[], int n) {
+	int i, j, temp;
+	for (i = n-1; i > 0; i-- {
+		for (j = 0; j < i; j++) {
+			if (dizi[j]>dizi[j+1])
+			{
+				temp = dizi[j];
+				dizi[j] = dizi[j+1];
+				dizi[j+1] = temp;
+			}
+		}
+	}
+}
